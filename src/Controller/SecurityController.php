89b20eec8a706f038703b4463914667dbed29a16
nicolas villetelle <nicolas.villetelle@adveris.fr>
2023-05-29 15:49:36 +0200

    logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -27,4 +27,9 @@ class SecurityController extends AbstractController
                'Location' => $iriConverter->getIriFromResource($user),
             ]);
     }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void{
+
+    }
 }
